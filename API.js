9d94627fc01f80fc6e0774cf59737d276551a899
gagnet <tim.madison.gagne@gmail.com>
2021-08-04 20:33:45 -0700

    heathstone api

--- API.js
+++ API.js
@@ -1,1 +1,14 @@
-test;
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/factions/%7Bfaction%7D",
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "aae9041da3msh758c6b4e4c4787fp1db3d8jsnfe04f26aaf30",
+    "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
